d94fbbf76f82c5c19fab5e478b40ebf778209ac0
zjh253860187 <2538600187@qq.com>
2020-07-03 15:52:31 +0800

    头部组件

---
+++ src/components/Header.vue
@@ -0,0 +1,21 @@
+<template>
+	<div id="header">
+		
+	</div>
+</template>
+
+<script>
+	export default{
+		name:'header',
+		data(){
+			return{
+				
+			}
+		}
+	}
+</script>
+
+<style scoped="less">
+	
+</style>
+
